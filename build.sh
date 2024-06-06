922359fc2a4585527db020f305be4e25d5e1ef69
Super User <root@localhost.localdomain>
2024-06-06 19:20:14 +0800

    update build

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-go build -mod=vendor -ldflags="-s -w" -o godnf ./cmd/godnf
+#yum install upx golang
+
+## -ldflags="-s -w"  Delete the symbol to reduce the size
+CGO_ENABLED=0 go build -mod=vendor -ldflags="-s -w" -o godnf ./cmd/godnf
+
+##Compression to reduce volume
+upx -9 godnf
+#go build -mod=vendor -ldflags="-s -w -extldflags '-static -lc -ldl'"  -o godnf ./cmd/godnf
