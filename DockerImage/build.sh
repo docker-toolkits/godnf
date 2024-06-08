70df5057b4b34dc5d248cac6d30abd733a86cef1
Super User <root@localhost.localdomain>
2024-06-08 21:41:35 +0800

    Update Docker Image name

--- DockerImage/build.sh
+++ DockerImage/build.sh
@@ -2,5 +2,5 @@ cd ../
 sh -x build.sh
 cd -
 cp ../godnf .
-docker build -t ocs23-godnf -f dockerfile.ocs23 .
-docker build -t ubi9-godnf -f dockerfile.ubi9 .
+docker build -t ocs23 -f dockerfile.ocs23 .
+docker build -t ubi9 -f dockerfile.ubi9 .
